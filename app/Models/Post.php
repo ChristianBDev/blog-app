f31ff5233dd2fd2c1d6d75e71b44216d7fd2234e
ChristianBDev <christianbennett67@gmail.com>
2023-03-25 22:29:05 -0500

    Episode 25/26 | Added posts to db instead seperate files

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,50 +2,19 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\ModelNotFoundException;
-use Illuminate\Support\Facades\File;
-use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
-class Post {
-    public $title;
-    public $excerpt;
-    public $date;
-    public $body;
-    public $slug;
+class Post extends Model
+{
+    use HasFactory;
 
-    public function __construct($title, $excerpt, $date, $body, $slug) {
-        $this->title = $title;
-        $this->excerpt = $excerpt;
-        $this->date = $date;
-        $this->body = $body;
-        $this->slug = $slug;
-    }
 
-    public static function all() {
-        return cache()->rememberForever('posts.all', fn () =>
-            collect(File::files(resource_path("posts")))
-                ->map(fn ($file) => YamlFrontMatter::parseFile($file))
-                ->map(fn ($document) => new Post(
-                    $document->title,
-                    $document->excerpt,
-                    $document->date,
-                    $document->body(),
-                    $document->slug
-                ))->sortByDesc('date')
-        );
-    }
+    protected $guarded = [];
+    // protected $fillable = ['title', 'excerpt', 'body'];
 
-    public static function find($slug) {
-        return static::all()->firstWhere('slug', $slug);
-    }
-
-    public static function findOrFail($slug) {
-        $post =  static::find($slug);
-
-        if (! $post) {
-            throw new ModelNotFoundException();
-        }
-
-        return $post;
+    public function category() {
+        return $this->belongsTo(Category::class);
     }
 }
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,7 +1,16 @@
 <x-layout>
     <article>
-        <h1>{{ $post->title }}</h1>
-        <div>{!! $post->body !!}</div>
+        <h1>
+            {{ $post->title }}
+        </h1>
+        <p>
+            <a href="/categories/{{ $post->category->slug }}">
+                {{ $post->category->name }}
+            </a>
+        </p>
+        <div>
+            {!! $post->body !!}
+        </div>
     </article>
     <a href="/">Go Back</a>
 </x-layout>
